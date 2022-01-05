98efcde9a1d77a45018c1a745dea38855b50e77b
Philippe Grassia <philippe.grassia@ligo.org>
2022-01-04 21:52:45 -0800

    cypress-setup script tuneup

--- cypress/cypress-setup.sh
+++ cypress/cypress-setup.sh
@@ -3,18 +3,10 @@
 if [ -d /e2e ] ; then
   cd /e2e
 fi
-pwd
-if [ -d node_modules ]; then
-  rm -rf node_modules
-fi
-
 apt update && apt install --no-install-recommends -y curl chromium xvfb npm wait-for-it
 mkdir -p /root/.cache/Cypress
 chmod -R 777 /root
-pwd
-ls -la
+
 npm install
-export CYPRESS_mailHogUrl=http://mailhog:8025
-export CYPRESS_baseUrl=https://dcc
-exec npm run test
+exec npm run test --spec cypress/integration/general/01-DocumentDatabase-admin.js
 
