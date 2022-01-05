ec79637535ccdce8bce5b385320c4cb180067e7c
Philippe Grassia <philippe.grassia@ligo.org>
2022-01-04 16:42:05 -0800

    cypress in CI

--- cypress/cypress-setup.sh
+++ cypress/cypress-setup.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-if [-d cypress ]; then
-  cd cypress
-elif [ -d /e2e ] ; then
+if [ -d /e2e ] ; then
   cd /e2e
 fi
-
+pwd
 if [ -d node_modules ]; then
   rm -rf node_modules
 fi
 
-apt update && apt install -y chromium xvfb npm
+apt update && apt install --no-install-recommends -y curl chromium xvfb npm wait-for-it
 mkdir -p /root/.cache/Cypress
 chmod -R 777 /root
-
+pwd
+ls -la
 npm install
 export CYPRESS_mailHogUrl=http://mailhog:8025
+export CYPRESS_baseUrl=https://dcc
 exec npm run test
 
